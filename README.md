5182cf3ca9564a44b505b0ffdc3645e7c6467d5e
vlad <>
2023-06-13 16:43:38 +0300

    linter prettier were added

--- README.md
+++ README.md
@@ -5,7 +5,15 @@
 - Typescript &nbsp;<img src="https://github.com/devicons/devicon/blob/master/icons/typescript/typescript-original.svg" title="Typescript" alt="Typescript" width="20" height="20"/>&nbsp;
 - Redux Toolkit Query &nbsp;<img src="https://github.com/devicons/devicon/blob/master/icons/redux/redux-original.svg" title="Redux" alt="Redux" width="20" height="20"/>&nbsp;
 - React Hook Form
+- Material UI, Emotion Styles
+- React Router
 - MSW
 - Vitest
 - React Testing Library
 - Test Coverage
+- Prettier, Linter Scripts
+
+- Husky
+- Docker
+
+- API use Node.js+Express Server
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,22 +1,22 @@
-import CustomForm from "@/components/form";
-import Header from "@/components/header";
-import { useGetMoviesListQuery } from "@/store/movies/api";
+import CustomForm from '@/components/form'
+import Header from '@/components/header'
+import { useGetMoviesListQuery } from '@/store/movies/api'
 
 const Main = () => {
-  const { data, isLoading } = useGetMoviesListQuery();
-  if (isLoading) return <div role="loader">Загрузка...</div>;
+  const { data, isLoading } = useGetMoviesListQuery()
+  if (isLoading) return <div role="loader">Загрузка...</div>
   return (
     <>
       <Header />
       <CustomForm />
       <div>main page</div>
       {data?.results?.map((movie) => (
-        <div role="movie-card" key={movie?.title || ""}>
-          <div>{movie?.title || ""}</div>
+        <div role="movie-card" key={movie?.title || ''}>
+          <div>{movie?.title || ''}</div>
         </div>
       ))}
     </>
-  );
-};
+  )
+}
 
-export default Main;
+export default Main
